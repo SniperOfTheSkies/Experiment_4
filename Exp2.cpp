c0fea1a991636024e58c4d5d1944561c25f81830
Raymond <raymonddaniel.ronquillo.eng@ust.edu.ph>
2019-07-09 00:09:39 +0800

    Problem6

--- Exp2.cpp
+++ Exp2.cpp
@@ -6,200 +6,45 @@ using namespace std;
 int main()
 
 {
+	int n, start = 1, end = 0;
 
-	int n;
+	cout << "                   Press any key to start the program... \n";
 
-	int Old, New, Max, Fib;
-
-	char ans;
-
-
-	cout << "               Press (0) to start the Fibonacci program... \n";
+	_getch();
 
 repeat:
 
-	cout << " User input: ";
+	cout << "     Add all numbers from 1 till: ";
 
 	cin >> n;
 
-	cout << "Initializing counting numbers... \n";
-
-	_getch();
-
-	cout << ".\n";
-
-	_getch();
-
-	cout << ".\n";
-
-	_getch();
-
-	cout << ".\n";
-
-	_getch();
 
 
-
-
-
-	cout << "\nGiven that they begin with: \n";
-
-	cout << "   0 & 1 \n\n";
-
-	cout << "The system would now continue after these numbers.\n" << "Press any key to continue counting... \n\n";
-
-	_getch();
-
-
-
-	switch (n)
+	for (int a = start; a <= n; a++)
 
 	{
-	case 0:
 
-		cout << "   1";
+		end += a;
 
-		_getch();
-
-		cout << ", 2";
-
-		_getch();
-
-		cout << ", 3";
-
-		_getch();
-
-		cout << ", 5";
-
-		_getch();
-
-		cout << ", 8";
-
-		_getch();
-
-		cout << ", 13";
-
-		_getch();
-
-		cout << ", 21";
-
-		_getch();
-
-		cout << ", 34";
-
-		_getch();
-
-		cout << ", 55";
-
-		_getch();
-
-		cout << ", 89";
-
-		_getch();
-
-		cout << ", 144";
-
-		_getch();
-
-		cout << ", 233";
-
-		_getch();
-
-		cout << ", 377";
-
-		_getch();
-
-		cout << ", 610";
-
-		_getch();
-
-		cout << ", 987";
-
-		_getch();
-
-		cout << ", 1597";
-
-		_getch();
-
-		cout << ", 2584";
-
-		_getch();
-
-		cout << "\n   4181";
-
-		_getch();
-
-		cout << ", 6765";
-
-		_getch();
-
-		cout << ", 10946";
-
-		_getch();
-
-		cout << "\n\nInitialization complete.\n\n";
-
-		cout << "Results: \n\n";
-
-		cout << "     All numbers from the 1st to the 22nd digit are successfully loaded. \n\n";
-
-
-
-		_getch();
-
-		cout << "     Checking log...\n\n";
-
-		_getch();
-
-		cout << ".\n";
-
-		_getch();
-
-		cout << ".\n";
-
-		_getch();
-
-		cout << ".\n";
-
-		_getch();
-
-		cout << "     No missing numbers found... \n\n";
-
-		cout << "     Press any key to end the program. \n\n";
-
-		break;
-
-	default:
-
-		cout << "  System Message: \n" << "      Since you've inputted an invalid number, \n      the system would force close. \n\n";
 	}
 
+	cout << "\n     The sum of all the numbers from 1 till " << n << " is equal to: \n\n          ";
 
+	cout << end;
 
-	cout << "Press any extraneous number to load the infinite fibonacci.\n(Till the number you will input)";
-
-	cin >> Max;
-
-	Old = 0;
-
-	New = 1;
-
-	Fib = Old + New;
-
-	cout << "\nLoading the Fibonacci: \n" << Old << " " << New;
+	cout << "\n";
 
 
 
-	while (Fib <= Max)
+	_getch();
 
-	{
-		cout << " " << Fib;
 
-		Old = New;
+	cout << "                    Press any key to end the program... \n";
 
-		New = Fib;
+	_getch();
 
-		Fib = Old + New;
-	}
+	cout << "Hooraaay! \n";
+
 	return 0;
+
 }
