dc140653c37ebfa0b6a34a3080c8b26d249e60b9
Raymond <raymonddaniel.ronquillo.eng@ust.edu.ph>
2019-07-09 00:01:42 +0800

    Problem2

--- Exp2.cpp
+++ Exp2.cpp
@@ -1,73 +1,51 @@
-#include<iostream>
-#include<conio.h>
+#include <iostream>
+#include <conio.h>
 
 using namespace std;
 
-int PackageA(int a)
-{
-	int r;
-	r = ((a - 10)*20)+995;
-	return r;
-}
-
-int PackageB(int a)
-{
-	int r;
-	r = ((a - 20) * 10)+1495;
-	return r;
-}
-
 int main()
-{
-	int x{}, z;
-	
-	while (x >= 0 )
-	{
-		char ans;
-		cout << "Package A: Php 995/mo, 10hrs of access are provided. Additional hours are Php 20/hr" << endl;
-		cout << "Package B: Php 1495/mo, 20hrs of access are provided. Additional hours are Php 10/hr" << endl;
-		cout << "Package A: Php 1995/mo, Unlimited access is provided" << endl;
-		cout << "" << endl;
-		cout << "What is your Package type: A, B, or C?: "; cin >> ans;
-		switch (ans)
-		{
-		case 'A':
-		case 'a':
-			cout << "What are your total number of hours used? "; cin >> x;
-			z = PackageA(x);
-			if (0<x<10)
-			{
-				cout << "Your total amount due is Php 995."<<endl;
-			}else
-			
-			cout << "Your total amount due is Php " << z<<".";
-			break;
-		case 'B':
-		case 'b':
-			z = PackageB(x);
-			cout << "What are your total number of hours used? "; cin >> x;
-			if ( x < 20)
-			{
-				cout << "Your total amount due is Php 1495."<<endl;
-			}else
-				
-			cout << "Your total amount due is Php " << z<<".";
-			break;
-		case 'C':
-		case 'c':
-			cout << "Your total amount due is Php 1995." << endl;
 
-		default:
-			cout << "You must choose between A, B, and C!" << endl;
-		}cout << ""<<endl;
+{
+
+	double gal, unpaid, wbill, wbilllate;
+	const int demandcharge = 35, late = 20;
+	cout << "                   Press any key to start the program... \n";
+
+	_getch();
+
+
+	cout << "To compute for your water bill, kindly answer the following questions: ";
+	_getch();
+
+	cout << "How many gallons of water did you consume? \n\n          ";
+
+	cin >> gal;
+
+	cout << "Do you have any unpaid balance? If so, kindly input here: \n(If none, kindly enter '0') \n\n          P ";
+	cin >> unpaid;
+
+
+
+	wbill = (gal * 1.10) + demandcharge;
+
+
+
+	if (unpaid > 0)
+
+	{
+		wbilllate = wbill + late;
+		cout << "Looks like you have more to pay! \nYour water bill for this month is P " << wbilllate;
 	}
+
+	else if (unpaid = 0)
+	{
+		cout << "Your water bill for this month is P " << wbill;
+	}
+
+	_getch();
+	cout << "\nKindly settle your account immediately!";
+	cout << "                   Press any key to end the program.\n";
+
 	_getch();
 	return 0;
 }
-
-
-
-
-
-
-
