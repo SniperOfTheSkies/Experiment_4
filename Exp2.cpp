23a3c49fc8fd536c779a3dbbef560a5ecd9bde8c
Raymond <raymonddaniel.ronquillo.eng@ust.edu.ph>
2019-07-09 00:07:58 +0800

    Problem5

--- Exp2.cpp
+++ Exp2.cpp
@@ -9,21 +9,18 @@ int main()
 
 	int n;
 
-	char a, ans;
+	int Old, New, Max, Fib;
 
-	cout << "                Press any key to start the counting program... \n";
+	char ans;
 
 
-	_getch();
+	cout << "               Press (0) to start the Fibonacci program... \n";
 
 repeat:
 
-	cout << "Would you like to start with 1 or 0?\n(You can enter a different number)\n";
-
-	cout << "\n Input: ";
+	cout << " User input: ";
 
 	cin >> n;
-
 
 	cout << "Initializing counting numbers... \n";
 
@@ -43,7 +40,13 @@ repeat:
 
 
 
-	cout << "\nPress any key to count each number.\n\n";
+
+
+	cout << "\nGiven that they begin with: \n";
+
+	cout << "   0 & 1 \n\n";
+
+	cout << "The system would now continue after these numbers.\n" << "Press any key to continue counting... \n\n";
 
 	_getch();
 
@@ -52,8 +55,7 @@ repeat:
 	switch (n)
 
 	{
-
-	case 1:
+	case 0:
 
 		cout << "   1";
 
@@ -67,19 +69,7 @@ repeat:
 
 		_getch();
 
-		cout << ", 4";
-
-		_getch();
-
 		cout << ", 5";
-
-		_getch();
-
-		cout << ", 6";
-
-		_getch();
-
-		cout << ", 7";
 
 		_getch();
 
@@ -87,51 +77,63 @@ repeat:
 
 		_getch();
 
-		cout << ", 9";
+		cout << ", 13";
 
 		_getch();
 
-		cout << ", 10";
+		cout << ", 21";
 
 		_getch();
 
-		cout << ", 12";
+		cout << ", 34";
 
 		_getch();
 
-		cout << ", 14";
+		cout << ", 55";
 
 		_getch();
 
-		cout << ", 16";
+		cout << ", 89";
 
 		_getch();
 
-		cout << ", 18";
+		cout << ", 144";
 
 		_getch();
 
-		cout << ", 20";
+		cout << ", 233";
 
 		_getch();
 
-		cout << ", 22";
+		cout << ", 377";
 
 		_getch();
 
-		cout << ", 24";
+		cout << ", 610";
 
 		_getch();
 
-		cout << ", 26";
+		cout << ", 987";
 
 		_getch();
 
-		cout << ", 28";
+		cout << ", 1597";
 
 		_getch();
 
-		cout << ", 30";
+		cout << ", 2584";
+
+		_getch();
+
+		cout << "\n   4181";
+
+		_getch();
+
+		cout << ", 6765";
+
+		_getch();
+
+		cout << ", 10946";
 
 		_getch();
 
@@ -139,11 +141,9 @@ repeat:
 
 		cout << "Results: \n\n";
 
-		cout << "     Numbers 1 - 10 successfully loaded. \n\n";
+		cout << "     All numbers from the 1st to the 22nd digit are successfully loaded. \n\n";
 
-		cout << "     Numbers 11 - 20 were unsuccessful. \n\n";
 
-		cout << "     Numbers 21 - 30 were unsuccessful. \n\n";
 
 		_getch();
 
@@ -163,200 +163,43 @@ repeat:
 
 		_getch();
 
-		cout << "     Missing numbers loading... \n\n";
+		cout << "     No missing numbers found... \n\n";
 
-		getch();
-
-		cout << "          - 11, 13, 15, 17, 19, 21, 23, 25, 27, 29 \n";
-
-		break;
-
-	case 0:
-
-		cout << "   0";
-
-		_getch();
-
-		cout << ", 1";
-
-		_getch();
-
-		cout << ", 2";
-
-		_getch();
-
-		cout << ", 3";
-
-		_getch();
-
-		cout << ", 4";
-
-		_getch();
-
-		cout << ", 5";
-
-		_getch();
-
-		cout << ", 6";
-
-		_getch();
-
-		cout << ", 7";
-
-		_getch();
-
-		cout << ", 8";
-
-		_getch();
-
-		cout << ", 9";
-
-		_getch();
-
-		cout << ", 10";
-
-		_getch();
-
-		cout << ", 12";
-
-		_getch();
-
-		cout << ", 14";
-
-		_getch();
-
-		cout << ", 16";
-
-		_getch();
-
-		cout << ", 18";
-
-		_getch();
-
-		cout << ", 20";
-
-		_getch();
-
-		cout << ", 22";
-
-		_getch();
-
-		cout << ", 24";
-
-		_getch();
-
-		cout << ", 26";
-
-		_getch();
-
-		cout << ", 28";
-
-		_getch();
-
-		cout << ", 30";
-
-		_getch();
-
-		cout << "\n\nInitialization complete.\n\n";
-
-		cout << "Results: \n\n";
-
-		cout << "     Numbers 1 - 10 successfully loaded. \n\n";
-
-		cout << "     Numbers 11 - 20 were unsuccessful. \n\n";
-
-		cout << "     Numbers 21 - 30 were unsuccessful. \n\n";
-
-		_getch();
-
-		cout << "     Checking log...\n\n";
-
-		_getch();
-
-		cout << ".\n";
-
-		_getch();
-
-		cout << ".\n";
-
-		_getch();
-
-		cout << ".\n";
-
-		_getch();
-
-		cout << "     Missing numbers loading... \n\n";
-
-		getch();
-
-		cout << "          > 11, 13, 15, 17, 19, 21, 23, 25, 27, 29 \n";
+		cout << "     Press any key to end the program. \n\n";
 
 		break;
 
 	default:
 
-		cout << "  System Message: \n" << "      Since you've inputted a different number, \n      the system would adjust to your input. \n\n";
-
-		_getch();
-
-		for (int a = 1; a <= 10; ++a)
-
-		{
-
-			cout << n + a << ", ";
-
-		}
-
-		for (int b = 1; b <= 10; ++b)
-
-		{
-
-			cout << 10 + n + b + b << ", ";
-
-		}
-
-		_getch();
-
-		cout << "\n\nInitialization complete.\n\n";
-
-		cout << "Results: \n\n";
-
-		cout << "     1st to 10th numbers: successfully loaded per number. \n\n";
-
-		cout << "     11th to 20th numbers: unsuccessfully loaded. \n\n";
-
-		_getch();
-
-		cout << "     Checking log...\n\n";
-
-		_getch();
-
-		cout << ".\n";
-
-		_getch();
-
-		cout << ".\n";
-
-		_getch();
-
-		cout << ".\n";
-
-		_getch();
-
-		cout << "     Numbers not found... \n\n";
-
-		getch();
-
-
-
-		cout << "Would you like to count again? (Y/N): ";
-
-		cin >> ans;
-
-		cout << "\n";
-
-		if (ans == 'y')
-
-			goto repeat;
-
+		cout << "  System Message: \n" << "      Since you've inputted an invalid number, \n      the system would force close. \n\n";
 	}
+
+
+
+	cout << "Press any extraneous number to load the infinite fibonacci.\n(Till the number you will input)";
+
+	cin >> Max;
+
+	Old = 0;
+
+	New = 1;
+
+	Fib = Old + New;
+
+	cout << "\nLoading the Fibonacci: \n" << Old << " " << New;
+
+
+
+	while (Fib <= Max)
+
+	{
+		cout << " " << Fib;
+
+		Old = New;
+
+		New = Fib;
+
+		Fib = Old + New;
+	}
+	return 0;
+}
