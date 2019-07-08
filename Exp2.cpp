c7792b5a3563ff64b8f466b081cea91f40092c9c
Raymond <raymonddaniel.ronquillo.eng@ust.edu.ph>
2019-07-09 00:03:46 +0800

    Problem3

--- Exp2.cpp
+++ Exp2.cpp
@@ -1,51 +1,97 @@
 #include <iostream>
 #include <conio.h>
+#include <math.h>
+#include <iomanip>
 
 using namespace std;
 
 int main()
 
 {
+	// 1
 
-	double gal, unpaid, wbill, wbilllate;
-	const int demandcharge = 35, late = 20;
+	int x, y;
+	double V;
+
+	// 2
+
+	double z = 2.5;
+
+
+	cout.setf(ios::fixed);
+
+	cout.setf(ios::showpoint);
+
+	//3
+
+	cout.precision(2);
+
 	cout << "                   Press any key to start the program... \n";
 
 	_getch();
 
 
-	cout << "To compute for your water bill, kindly answer the following questions: ";
-	_getch();
 
-	cout << "How many gallons of water did you consume? \n\n          ";
+	cout << "What is the value of your x: ";
 
-	cin >> gal;
+	cin >> x;
 
-	cout << "Do you have any unpaid balance? If so, kindly input here: \n(If none, kindly enter '0') \n\n          P ";
-	cin >> unpaid;
+	cout << "\n\nWhat is the value of your y: ";
 
+	cin >> y;
 
+	cout << "\n\nTake note that the value of Z = 2.5";
 
-	wbill = (gal * 1.10) + demandcharge;
+	cout << "Recap of Values: \n\n      X = " << x << "\n      Y = " << y << "\n      Z = 2.5";
 
+	cout << "\n\nPress any key to solve for V...\n";
 
-
-	if (unpaid > 0)
-
-	{
-		wbilllate = wbill + late;
-		cout << "Looks like you have more to pay! \nYour water bill for this month is P " << wbilllate;
-	}
-
-	else if (unpaid = 0)
-	{
-		cout << "Your water bill for this month is P " << wbill;
-	}
 
 	_getch();
-	cout << "\nKindly settle your account immediately!";
+
+
+
+	cout << "   Answer: \n\n";
+
+	// 3 & 4	
+
+	switch (x)
+
+	{
+
+	case 1:
+
+		if (y > 1 && y < 5)
+
+			cout << std::setw(10) << "V = " << x * y * z << "\n" << "\n      Where the formula used was x * y * z";
+
+		else if (y >= 5)
+
+			cout << std::setw(10) << "V = " << x + y / z << "\n" << "\n      Where the formula used was x + y / z";
+
+		break;
+
+	case 2:
+
+		if (y <= 5)
+
+			cout << std::setw(10) << "V = " << abs(x - y) / z << "\n" << "\n      Where the formula used was |(x - y)| / z";
+
+		else if (y > 5)
+
+			cout << std::setw(10) << "V = " << x - (sqrt(y + z)) << "\n" << "\n      Where the formula used was x - (the square root of (y + z))";
+
+		break;
+
+	default:
+
+		cout << std::setw(10) << "V = " << x + y + z << "\n" << "\n      Where the formula used was x + y + z";
+
+	}
+
 	cout << "                   Press any key to end the program.\n";
 
 	_getch();
+
 	return 0;
 }
