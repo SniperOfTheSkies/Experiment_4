e3bd294aaad48496c30bdf606e32421a7ceb469a
Raymond <raymonddaniel.ronquillo.eng@ust.edu.ph>
2019-07-09 00:06:10 +0800

    Problem4

--- Exp2.cpp
+++ Exp2.cpp
@@ -1,97 +1,362 @@
 #include <iostream>
 #include <conio.h>
-#include <math.h>
-#include <iomanip>
 
 using namespace std;
 
 int main()
 
 {
-	// 1
 
-	int x, y;
-	double V;
+	int n;
 
-	// 2
+	char a, ans;
 
-	double z = 2.5;
+	cout << "                Press any key to start the counting program... \n";
 
 
-	cout.setf(ios::fixed);
+	_getch();
 
-	cout.setf(ios::showpoint);
+repeat:
 
-	//3
+	cout << "Would you like to start with 1 or 0?\n(You can enter a different number)\n";
 
-	cout.precision(2);
+	cout << "\n Input: ";
 
-	cout << "                   Press any key to start the program... \n";
+	cin >> n;
+
+
+	cout << "Initializing counting numbers... \n";
+
+	_getch();
+
+	cout << ".\n";
+
+	_getch();
+
+	cout << ".\n";
+
+	_getch();
+
+	cout << ".\n";
 
 	_getch();
 
 
 
-	cout << "What is the value of your x: ";
-
-	cin >> x;
-
-	cout << "\n\nWhat is the value of your y: ";
-
-	cin >> y;
-
-	cout << "\n\nTake note that the value of Z = 2.5";
-
-	cout << "Recap of Values: \n\n      X = " << x << "\n      Y = " << y << "\n      Z = 2.5";
-
-	cout << "\n\nPress any key to solve for V...\n";
-
+	cout << "\nPress any key to count each number.\n\n";
 
 	_getch();
 
 
 
-	cout << "   Answer: \n\n";
-
-	// 3 & 4	
-
-	switch (x)
+	switch (n)
 
 	{
 
 	case 1:
 
-		if (y > 1 && y < 5)
+		cout << "   1";
 
-			cout << std::setw(10) << "V = " << x * y * z << "\n" << "\n      Where the formula used was x * y * z";
+		_getch();
 
-		else if (y >= 5)
+		cout << ", 2";
 
-			cout << std::setw(10) << "V = " << x + y / z << "\n" << "\n      Where the formula used was x + y / z";
+		_getch();
+
+		cout << ", 3";
+
+		_getch();
+
+		cout << ", 4";
+
+		_getch();
+
+		cout << ", 5";
+
+		_getch();
+
+		cout << ", 6";
+
+		_getch();
+
+		cout << ", 7";
+
+		_getch();
+
+		cout << ", 8";
+
+		_getch();
+
+		cout << ", 9";
+
+		_getch();
+
+		cout << ", 10";
+
+		_getch();
+
+		cout << ", 12";
+
+		_getch();
+
+		cout << ", 14";
+
+		_getch();
+
+		cout << ", 16";
+
+		_getch();
+
+		cout << ", 18";
+
+		_getch();
+
+		cout << ", 20";
+
+		_getch();
+
+		cout << ", 22";
+
+		_getch();
+
+		cout << ", 24";
+
+		_getch();
+
+		cout << ", 26";
+
+		_getch();
+
+		cout << ", 28";
+
+		_getch();
+
+		cout << ", 30";
+
+		_getch();
+
+		cout << "\n\nInitialization complete.\n\n";
+
+		cout << "Results: \n\n";
+
+		cout << "     Numbers 1 - 10 successfully loaded. \n\n";
+
+		cout << "     Numbers 11 - 20 were unsuccessful. \n\n";
+
+		cout << "     Numbers 21 - 30 were unsuccessful. \n\n";
+
+		_getch();
+
+		cout << "     Checking log...\n\n";
+
+		_getch();
+
+		cout << ".\n";
+
+		_getch();
+
+		cout << ".\n";
+
+		_getch();
+
+		cout << ".\n";
+
+		_getch();
+
+		cout << "     Missing numbers loading... \n\n";
+
+		getch();
+
+		cout << "          - 11, 13, 15, 17, 19, 21, 23, 25, 27, 29 \n";
 
 		break;
 
-	case 2:
+	case 0:
 
-		if (y <= 5)
+		cout << "   0";
 
-			cout << std::setw(10) << "V = " << abs(x - y) / z << "\n" << "\n      Where the formula used was |(x - y)| / z";
+		_getch();
 
-		else if (y > 5)
+		cout << ", 1";
 
-			cout << std::setw(10) << "V = " << x - (sqrt(y + z)) << "\n" << "\n      Where the formula used was x - (the square root of (y + z))";
+		_getch();
+
+		cout << ", 2";
+
+		_getch();
+
+		cout << ", 3";
+
+		_getch();
+
+		cout << ", 4";
+
+		_getch();
+
+		cout << ", 5";
+
+		_getch();
+
+		cout << ", 6";
+
+		_getch();
+
+		cout << ", 7";
+
+		_getch();
+
+		cout << ", 8";
+
+		_getch();
+
+		cout << ", 9";
+
+		_getch();
+
+		cout << ", 10";
+
+		_getch();
+
+		cout << ", 12";
+
+		_getch();
+
+		cout << ", 14";
+
+		_getch();
+
+		cout << ", 16";
+
+		_getch();
+
+		cout << ", 18";
+
+		_getch();
+
+		cout << ", 20";
+
+		_getch();
+
+		cout << ", 22";
+
+		_getch();
+
+		cout << ", 24";
+
+		_getch();
+
+		cout << ", 26";
+
+		_getch();
+
+		cout << ", 28";
+
+		_getch();
+
+		cout << ", 30";
+
+		_getch();
+
+		cout << "\n\nInitialization complete.\n\n";
+
+		cout << "Results: \n\n";
+
+		cout << "     Numbers 1 - 10 successfully loaded. \n\n";
+
+		cout << "     Numbers 11 - 20 were unsuccessful. \n\n";
+
+		cout << "     Numbers 21 - 30 were unsuccessful. \n\n";
+
+		_getch();
+
+		cout << "     Checking log...\n\n";
+
+		_getch();
+
+		cout << ".\n";
+
+		_getch();
+
+		cout << ".\n";
+
+		_getch();
+
+		cout << ".\n";
+
+		_getch();
+
+		cout << "     Missing numbers loading... \n\n";
+
+		getch();
+
+		cout << "          > 11, 13, 15, 17, 19, 21, 23, 25, 27, 29 \n";
 
 		break;
 
 	default:
 
-		cout << std::setw(10) << "V = " << x + y + z << "\n" << "\n      Where the formula used was x + y + z";
+		cout << "  System Message: \n" << "      Since you've inputted a different number, \n      the system would adjust to your input. \n\n";
+
+		_getch();
+
+		for (int a = 1; a <= 10; ++a)
+
+		{
+
+			cout << n + a << ", ";
+
+		}
+
+		for (int b = 1; b <= 10; ++b)
+
+		{
+
+			cout << 10 + n + b + b << ", ";
+
+		}
+
+		_getch();
+
+		cout << "\n\nInitialization complete.\n\n";
+
+		cout << "Results: \n\n";
+
+		cout << "     1st to 10th numbers: successfully loaded per number. \n\n";
+
+		cout << "     11th to 20th numbers: unsuccessfully loaded. \n\n";
+
+		_getch();
+
+		cout << "     Checking log...\n\n";
+
+		_getch();
+
+		cout << ".\n";
+
+		_getch();
+
+		cout << ".\n";
+
+		_getch();
+
+		cout << ".\n";
+
+		_getch();
+
+		cout << "     Numbers not found... \n\n";
+
+		getch();
+
+
+
+		cout << "Would you like to count again? (Y/N): ";
+
+		cin >> ans;
+
+		cout << "\n";
+
+		if (ans == 'y')
+
+			goto repeat;
 
 	}
-
-	cout << "                   Press any key to end the program.\n";
-
-	_getch();
-
-	return 0;
-}
